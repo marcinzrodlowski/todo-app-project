576574fcd31cda977b6edda2e1a877e0cfed1b16
marcinzrodlowski <formyriadofsetups@gmail.com>
2024-02-21 12:52:18 +0100

    add project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { UserWarning } from './UserWarning';
+import { AppContainer } from './AppContainer';
 
-function App() {
+const USER_ID = 12121;
+
+export const App = () => {
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer />
   );
-}
-
-export default App;
+};
